c85605e4c40edc0912c366c9c3dd88873bede621
xaivntaajher <dn_her@yahoo.com>
2023-08-30 12:01:11 -0500

    create routes and req

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     console.log("Here")
-    res.status(500).send("Hi")
+    res.render('index', {text: "World"})
 })
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
 
 app.listen(3000)
